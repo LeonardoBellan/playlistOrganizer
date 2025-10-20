6e567fdae9a8347d7c4d4744a8098920c1a6caff
Bell <leonardobellan2005@gmail.com>
2025-10-20 10:28:41 +0200

    Clack start

--- src/index.js
+++ src/index.js
@@ -2,23 +2,28 @@
 
 import dotenv from "dotenv";
 import { sortPlaylists } from "./sortPlaylist.js";
+import { intro, outro } from "@clack/prompts";
 
 //Setup
-const program = new Command();
 dotenv.config();
 
+intro("Inizio app");
 //Playlists IDs
 const PLAYLIST_IDs = process.argv.slice(2);
 
-/*(async () => {
-        //If there are no arguments, use the test playlist
-        if (PLAYLIST_IDs.length === 0) {
-            console.log("\x1b[33mTest playlist\x1b[0m");
-            PLAYLIST_IDs.push(process.env.TEST_PLAYLIST_ID);
+await sortPlaylists(PLAYLIST_IDs);
 
-            //Shuffle the playlist randomly
+outro("Fine app");
+
+/*(async () => {
+    //If there are no arguments, use the test playlist
+    if (PLAYLIST_IDs.length === 0) {
+        console.log("\x1b[33mTest playlist\x1b[0m");
+        PLAYLIST_IDs.push(process.env.TEST_PLAYLIST_ID);
+
+        //Shuffle the playlist randomly
         //await shufflePlaylist(PLAYLIST_IDs[0]);
-        }
+    }
 
     await sortPlaylists(PLAYLIST_IDs);
 })();
